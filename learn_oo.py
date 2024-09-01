8f7215fbc6704be0dff6db2ad7fe465653dec8d1
legoperson <64126997+legoperson@users.noreply.github.com>
2024-09-01 17:10:52 +1000

    Update learn_oo.py

--- learn_oo.py
+++ learn_oo.py
@@ -65,8 +65,9 @@ if st.session_state.start_time is not None:
             st.session_state.current_text = "列表为空或索引超出范围"
         st.session_state.start_time = current_time  # 重置时间
 
-    st.write(st.session_state.current_text)
-
+    text = st.session_state.current_text
+    font_size = 24
+    st.markdown(f'<p style="font-size:{font_size}px;">{text}</p>', unsafe_allow_html=True)
     # 自动刷新以便于5秒钟更新一次
     time.sleep(1)
     st.rerun()
